1fde1971be60565ad97aa23ed2c14d5a3eec417f
Justin Nipper <jnipper-t@fabkids.com>
2018-07-11 20:04:02 -0700

    added some kick ass feature

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -22,26 +22,33 @@ export default {
 
 <style lang="scss">
 .button {
-	cursor: pointer;
-	outline: none;
-	&.red {
-		border: 5px solid red;
-		border-radius: 50%;
-		background: linear-gradient(to bottom right, darkred,red);
-		height: 80px;
-		width: 80px;
-	}
-	&.green {
-		border: 5px solid green;
-		border-radius: 50%;
-		background: linear-gradient(to bottom right, darkgreen,green);
-		height: 80px;
-		width: 80px;
-	}
-	&.plain {
-		background: #ccc;
-		padding: 10px 20px;
-		text-align: center;
-	}
+  cursor: pointer;
+  outline: none;
+  &.red {
+    border: 5px solid red;
+    border-radius: 50%;
+    background: linear-gradient(to bottom right, darkred, red);
+    height: 80px;
+    width: 80px;
+  }
+  &.green {
+    border: 5px solid green;
+    border-radius: 50%;
+    background: linear-gradient(to bottom right, darkgreen, green);
+    height: 80px;
+    width: 80px;
+  }
+  &.gold {
+    border: 5px solid gold;
+    border-radius: 50%;
+    background: linear-gradient(to bottom right, goldenrod, gold);
+    height: 80px;
+    width: 80px;
+  }
+  &.plain {
+    background: #ccc;
+    padding: 10px 20px;
+    text-align: center;
+  }
 }
 </style>
